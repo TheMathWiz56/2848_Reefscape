d1c9d26afcb0e6ad0461f67e740e808ead00e3ea
william2848 <27164@jcpstudents.org>
2025-01-11 15:02:21 -0600

    abcd

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,12 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.subsystems.Arm;
@@ -15,13 +17,17 @@ import frc.robot.subsystems.Arm;
 public class RobotContainer {
   private final Arm arm = new Arm();
   private final Trigger updated_reference = new Trigger(()->arm.is_reference_updated());
+  private CommandGenericHID keypad = new CommandGenericHID(0);
+  
+  
 
   SendableChooser<Command> m_chooser = new SendableChooser<>();
 
   private void configureBindings() {
     // updated_reference.onTrue(arm.go_to_reference()); Disabled for manual testing
     arm.setDefaultCommand(arm.hold_position());
-
+    keypad.button(0).onTrue(arm.go_to_reference(.1));
+    keypad.button(0).onFalse(arm.go_to_reference(.2));
     m_chooser.setDefaultOption("None", Commands.idle(arm));
     m_chooser.addOption("Q_Forward", arm.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
     m_chooser.addOption("Q_Reverse", arm.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
