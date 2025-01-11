850ea9a93b8818d2b0b8c3ec471632c7beb39eb3
Jesuit Student 26317 <26317@jcpstudents.org>
2025-01-11 17:19:18 -0600

    Elevator devices

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -6,11 +6,26 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
   
+    SparkMax elevatorMotor1 = new SparkMax(0, MotorType.kBrushless);
+    SparkMax elevatorMotor2 = new SparkMax(0, MotorType.kBrushless);
+
+    //Lidar sensor - could be a serial bus input instead
+    AnalogInput lidar = new AnalogInput(0);
+    
+    //Limit switches
+    DigitalInput limitSwitchTop = new DigitalInput(0);
+    DigitalInput limitSwitchBottom = new DigitalInput(0); 
+
   public Elevator() {
 
   }
