53f4f48a454211259660f6d5ab66d6c168c639ab
Jesuit Student 26317 <26317@jcpstudents.org>
2025-01-11 15:30:29 -0600

    Limelight port forwarding

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -15,6 +16,14 @@ public class Robot extends TimedRobot {
 
   public Robot() {
     m_robotContainer = new RobotContainer();
+
+    //Limelight port forwarding
+    for (int port = 5800; port <= 5809; port++) {
+      PortForwarder.add(port, "limelight-front.local", port);
+    }
+    for (int port = 5800; port <= 5809; port++) {
+      PortForwarder.add(port+10, "limelight-back.local", port);
+    }    
   }
 
   @Override
