4ebc39e3d73b3f3b279089d89e0106744002a122
Jesuit Student 26317 <26317@jcpstudents.org>
2025-01-14 17:41:36 -0600

    Define classes for arm, ascender, groundalgae. Create a constants file

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,30 @@
+package frc.robot;
+
+public final class Constants {
+
+    //Constants relating to the drivetrain are probably in TunerConstants
+
+    public static class ControllerConstants {
+        public static final int kDriverController = 0;
+        //One of the operator controllers is going to be for emergencies if all else fails
+        public static final int kOperatorController1 = 1;
+        public static final int kOperatorController2 = 2;
+    }
+
+    public static class ArmConstants {
+
+    }
+
+    public static class AscenderConstants {
+
+    }
+
+    public static class ElevatorConstants {
+
+    }
+
+    public static class GroundAlgaeConstants {
+
+    }
+
+}
---
+++ src/main/java/frc/robot/subsystems/Ascender.java
@@ -0,0 +1,26 @@
+//Subsystem template
+
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Ascender extends SubsystemBase {
+  public Ascender() {
+
+  }
+
+  @Override
+  public void periodic() {
+
+  }
+
+  @Override
+  public void simulationPeriodic() {
+
+  }
+}
