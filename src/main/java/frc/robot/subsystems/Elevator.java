f1a93293c8463f66ae2d2beb9a15422f5e544743
TheMathWiz56 <146789934+TheMathWiz56@users.noreply.github.com>
2025-01-16 10:47:10 -0600

    Limit switch behavior and sendable

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -28,7 +28,8 @@ import frc.robot.Constants.ElevatorConstants;
  * profile. Look at the arm branch for some ideas of how to imlpement feedforward, I can also help.
  * - You can either use methods like public void setMotors ()... or you can use public Command setMotors() with command
  * factories to simplify code and remove boiler plate code
- * - 
+ * - Add a sendable builder and put all sensor information, motor outputs, setpoints, setpoints errors, PID outputs, Feedforward outputs etc.
+ * in periodic send the sendable object to the dashboard for debugging and logging
  */
 
 public class Elevator extends SubsystemBase {
@@ -40,6 +41,11 @@ public class Elevator extends SubsystemBase {
   LaserCan laserCan = new LaserCan(ElevatorConstants.kElevatorLaserCanId);
 
   // Limit switches
+  /*Should be bound to a trigger in robotcontainer that calls something like public Command atHardLimit()
+  Which 0's the motor output if it is trying to drive the elevator into an unsafe state or allows the elevator to move
+  if the motors are trying to move it to a safe state. 
+  ie. we are at the Bottom limit switch and trying to drive the elevator down more, don't allow motor output
+  if we are at the bottom limit switch and tring to drive the elevator up, allow motor output */
   private final DigitalInput limitSwitchTop = new DigitalInput(ElevatorConstants.kElevatorLimitSwitchTopId);
   private final DigitalInput limitSwitchBottom = new DigitalInput(ElevatorConstants.kElevatorLimitSwitchBottomId);
 
