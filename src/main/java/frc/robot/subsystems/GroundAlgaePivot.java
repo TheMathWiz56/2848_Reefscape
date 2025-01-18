fdba79d13c8cbc5219385507fd6577ea3a69b7ce
Jesuit Student 26317 <26317@jcpstudents.org>
2025-01-18 15:04:01 -0600

    GroundAlgaePivot objects

--- src/main/java/frc/robot/subsystems/GroundAlgaePivot.java
+++ src/main/java/frc/robot/subsystems/GroundAlgaePivot.java
@@ -5,18 +5,31 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.GroundAlgaePivotConstants;
 
 public class GroundAlgaePivot extends SubsystemBase {
 
+    // Spark Max and Encoder
     private final SparkMax pivotMotor = new SparkMax(GroundAlgaePivotConstants.kMotorId,
             MotorType.kBrushless);
     private final AbsoluteEncoder pivotEncoder = pivotMotor.getAbsoluteEncoder();
     private final SparkMaxConfig pivotMotorConfig = new SparkMaxConfig();
 
-    public GroundAlgaePivot() {
+    // Arm Feedforward
+    private final ArmFeedforward pivotFeedforward = new ArmFeedforward(0, 0, 0, 0, 0);
 
+    // Trapezoid Motion Profile
+    private final TrapezoidProfile pivotTrapezoidProfile = new TrapezoidProfile(new TrapezoidProfile.Constraints(0, 0));
+    private final TrapezoidProfile.State pivotTrapezoidInitial = new TrapezoidProfile.State();
+    private final TrapezoidProfile.State pivotTrapezoidFinal = new TrapezoidProfile.State();
+
+    public GroundAlgaePivot() {
+        
+
+        
     }
 
     @Override
