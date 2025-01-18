0d8ef0afddab4c308c2d3a442a337ef7f69b6a07
Jesuit Student 26317 <26317@jcpstudents.org>
2025-01-18 15:59:08 -0600

    Provisional Lidar class

---
+++ src/main/java/frc/robot/subsystems/Lidar.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Lidar extends SubsystemBase {
+
+    // TODO: Code to detect coral and algae on the reef. Not sure if this should be
+    // in its own subsystem or in a different subsystem (maybe the pincer?)
+
+}
