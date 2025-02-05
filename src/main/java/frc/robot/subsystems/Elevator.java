f5df9af00aef219a8461057c7f59a10af5ef42ea
Jesuit Student 26317 <26317@jcpstudents.org>
2025-02-04 19:11:11 -0600

    Add changeable pid

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -18,19 +18,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.ElevatorConstants.*;
 
-/*
- * Joseph Notes
- * - [Done] one of the elevator motors should be configured to follow the other, not just setting them both to the same output
- * - [Half finished] If we decide to use the internal encoder or an absolute encoder to measure the elevator's position we can use the internal
- * pid controller on the elevator. 
- * - [Done for the LaserCan setup] Should also have a feedforward controller for the elevator, results in smoothing motion since it allows you to create a motion
- * profile. Look at the arm branch for some ideas of how to imlpement feedforward, I can also help.
- * - [Added 2 commands] You can either use methods like public void setMotors ()... or you can use public Command setMotors() with command
- * factories to simplify code and remove boiler plate code
- * - [Done for LaserCan] Add a sendable builder and put all sensor information, motor outputs, setpoints, setpoints errors, PID outputs, Feedforward outputs etc.
- * in periodic send the sendable object to the dashboard for debugging and logging
- */
-
 public class Elevator extends SubsystemBase {
 
   private final TalonFX elevatorMotor = new TalonFX(kMotorId);
@@ -38,19 +25,6 @@ public class Elevator extends SubsystemBase {
   // Motor configs
   TalonFXConfiguration elevatorMotorConfig = new TalonFXConfiguration();
 
-  // Limit switches
-  /*
-   * [Done] Should be bound to a trigger in robotcontainer that calls something
-   * like
-   * public Command atHardLimit()
-   * Which 0's the motor output if it is trying to drive the elevator into an
-   * unsafe state or allows the elevator to move
-   * if the motors are trying to move it to a safe state.
-   * ie. we are at the Bottom limit switch and trying to drive the elevator down
-   * more, don't allow motor output
-   * if we are at the bottom limit switch and tring to drive the elevator up,
-   * allow motor output
-   */
   private final DigitalInput elevatorLimitSwitchTop = new DigitalInput(kLimitSwitchTopId);
   private final DigitalInput elevatorLimitSwitchBottom = new DigitalInput(
       kLimitSwitchBottomId);
@@ -77,19 +51,20 @@ public class Elevator extends SubsystemBase {
 
     // Set motor configurations
     elevatorMotorConfig.MotorOutput
-      .withInverted(kMotorInverted)
-      .withNeutralMode(kMotorIdleMode);
+        .withInverted(kMotorInverted)
+        .withNeutralMode(kMotorIdleMode);
 
     elevatorMotorConfig.CurrentLimits
-      .withStatorCurrentLimit(kCurrentLimit);
+        .withStatorCurrentLimit(kCurrentLimit);
 
-      //Missing feedback sensor, izone, imaxaccum, outputrange
+    // Missing feedback sensor, izone, imaxaccum, outputrange
     elevatorMotorConfig.Slot0
-      .withKP(kP)
-      .withKI(kI)
-      .withKD(kD);
-    
-      //Missing encoder position conversion factor, velocity conversion factor, zero offset
+        .withKP(kP)
+        .withKI(kI)
+        .withKD(kD);
+
+    // Missing encoder position conversion factor, velocity conversion factor, zero
+    // offset
 
     // Timer start
     timer.start();
@@ -105,7 +80,8 @@ public class Elevator extends SubsystemBase {
   public void setElevatorSetpoint(double setpoint) {
     currentSetpoint = setpoint;
     timer.reset();
-    startState = new TrapezoidProfile.State(elevatorMotor.getPosition().getValueAsDouble(), elevatorMotor.getVelocity().getValueAsDouble());
+    startState = new TrapezoidProfile.State(elevatorMotor.getPosition().getValueAsDouble(),
+        elevatorMotor.getVelocity().getValueAsDouble());
     goalState = new TrapezoidProfile.State(setpoint, 0.0);
   }
 
@@ -114,9 +90,10 @@ public class Elevator extends SubsystemBase {
     // Units might be messed up
     currentState = elevatorTrapezoidProfile.calculate(0.02, currentState, goalState);
     PositionVoltage request = new PositionVoltage(0).withSlot(0)
-      .withPosition(currentState.position)
-      .withVelocity(currentState.velocity)
-      .withFeedForward(feedforward.calculate(elevatorMotor.getVelocity().getValueAsDouble(), elevatorMotor.getAcceleration().getValueAsDouble()));
+        .withPosition(currentState.position)
+        .withVelocity(currentState.velocity)
+        .withFeedForward(feedforward.calculate(elevatorMotor.getVelocity().getValueAsDouble(),
+            elevatorMotor.getAcceleration().getValueAsDouble()));
 
     elevatorMotor.setControl(request);
   }
@@ -135,7 +112,8 @@ public class Elevator extends SubsystemBase {
     return this.run(() -> setMotorVoltage(0.0)).withName("Elevator At Top Limit");
   }
 
-  // Zero encoders and lift elevator slightly, triggered by limit switches in RobotContainer
+  // Zero encoders and lift elevator slightly, triggered by limit switches in
+  // RobotContainer
   public Command elevatorAtBottomLimit() {
     return this.startRun(() -> zeroEncoder(), () -> setMotorVoltage(1.0)).withName("Elevator At Bottom Limit");
   }
@@ -147,14 +125,13 @@ public class Elevator extends SubsystemBase {
     }).withName("Elevator Default Command");
   }
 
-
   // Command to go to position
   public Command goToPosition(double position, String positionName) {
     return this.startRun(() -> {
       setElevatorSetpoint(position);
     }, () -> {
-        currentState = elevatorTrapezoidProfile.calculate(timer.get(), startState, goalState);
-        setMotorOutput(currentState.position, currentState.velocity);
+      currentState = elevatorTrapezoidProfile.calculate(timer.get(), startState, goalState);
+      setMotorOutput(currentState.position, currentState.velocity);
     }).until(() -> elevatorTrapezoidProfile.isFinished(timer.get()))
         .withName("Go to " + positionName);
   }
@@ -203,11 +180,26 @@ public class Elevator extends SubsystemBase {
     super.initSendable(builder); // Not sure why we need this
 
     // Motor information
-    builder.addDoubleProperty("Elevator Motor Temperature", () -> elevatorMotor.getDeviceTemp().getValueAsDouble(), null);
-    builder.addDoubleProperty("Elevator Motor Closed Loop Output", () -> elevatorMotor.getClosedLoopOutput().getValueAsDouble(), null);
-    builder.addDoubleProperty("Elevator Motor Output Current", () -> elevatorMotor.getSupplyCurrent().getValueAsDouble(), null);
+    builder.addDoubleProperty("Elevator Motor Temperature", () -> elevatorMotor.getDeviceTemp().getValueAsDouble(),
+        null);
+    builder.addDoubleProperty("Elevator Motor Closed Loop Output",
+        () -> elevatorMotor.getClosedLoopOutput().getValueAsDouble(), null);
+    builder.addDoubleProperty("Elevator Motor Output Current",
+        () -> elevatorMotor.getSupplyCurrent().getValueAsDouble(), null);
 
     // Add closed loop information (not done yet)
+    builder.addDoubleProperty("Elevator kP,", () -> elevatorMotorConfig.Slot0.kP, (kPNew) -> {
+      elevatorMotorConfig.Slot0.withKP(0);
+      elevatorMotor.getConfigurator().apply(elevatorMotorConfig);
+    });
+    builder.addDoubleProperty("Elevator kI,", () -> elevatorMotorConfig.Slot0.kI, (kINew) -> {
+      elevatorMotorConfig.Slot0.withKI(0);
+      elevatorMotor.getConfigurator().apply(elevatorMotorConfig);
+    });
+    builder.addDoubleProperty("Elevator kD,", () -> elevatorMotorConfig.Slot0.kD, (kDNew) -> {
+      elevatorMotorConfig.Slot0.withKD(0);
+      elevatorMotor.getConfigurator().apply(elevatorMotorConfig);
+    });
 
     // Feedforward values
     // There doesn't seem to be any setters for these. Don't think these would be
