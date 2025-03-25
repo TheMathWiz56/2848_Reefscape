8ade12e563113022cdd6b6ade33c87a84f4930cd
TheMathWiz56 <146789934+TheMathWiz56@users.noreply.github.com>
2025-03-24 19:13:38 -0500

    final tournmane tupdate

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -299,7 +299,7 @@ public final class Constants {
             public static final FeedbackSensor kPincerMotorFeedbackSensor = FeedbackSensor.kAbsoluteEncoder;
             public static final double kPincerMotorMinOutput = -1;
             public static final double kPincerMotorMaxOutput = 1;
-            public static final double kPincerMotorAbsoluteEncoderOffset = 0.7;
+            public static final double kPincerMotorAbsoluteEncoderOffset = 0.8380989;
             public static final boolean kPincerMotorZeroCentered = true;
 
             // Soft Limits
@@ -383,8 +383,8 @@ public final class Constants {
             public static final double kSetpointStow = -0.065;
             public static final double kSetpointStowCoral = -12.0;//-6.130;
             public static final double kSetpointStowAlgae = -5.0;
-            public static final double kSetpointReefAlgaeHigh = -22.916;
-            public static final double kSetpointReefAlgaeLow = -14.299;
+            public static final double kSetpointReefAlgaeHigh = -23.416;
+            public static final double kSetpointReefAlgaeLow = -14.799;
             public static final double kSetpointGroundAlgae = -2.0;
             public static final double kSetpointProcessor = -4.5;
             public static final double kSetpointNet = -42.0;
