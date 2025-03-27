584d3060e9a85937049e9ad046cc0d23b4382b94
william2848 <27164@jcpstudents.org>
2025-03-27 16:21:38 -0500

    added center align command back

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -183,11 +183,11 @@ public class RobotContainer {
                                 .andThen(Commands.run(() -> drivetrain.setControl(new SwerveRequest.RobotCentric().withVelocityX(0.45)), drivetrain))
                                 .until(() -> !pincer.hasCoral() || manualDrivebase.getAsBoolean())
                                 );
-                /*driverJoystick.leftTrigger(.5)
+                driverJoystick.leftTrigger(.5)
                         .and(LLHasTag)
                                 .onTrue(drivetrain.pathPIDToTagMiddleSelect()
                                 .andThen(Commands.run(() -> drivetrain.setControl(new SwerveRequest.RobotCentric().withVelocityX(0.3)), drivetrain))
-                                .until(() -> pincer.hasAlgae() || manualDrivebase.getAsBoolean()));*/
+                                .until(() -> pincer.hasAlgae() || manualDrivebase.getAsBoolean()));
                 // Make elevator go up a set amount - very not working
                 // driverJoystick.y().onTrue(elevator.goUpByDistance(1.25));
 
