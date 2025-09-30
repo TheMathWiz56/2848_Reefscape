b2e1674edf451027163e06891c52f2ae4f538830
Jesuit Student 26317 <26317@jcpstudents.org>
2025-09-30 17:37:16 -0500

    Uncomment exhaust net command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -437,10 +437,10 @@ public class RobotContainer {
         operatorJoystick.pov(180).onTrue(commandFactory.groundAlgae());
         keypad.button(8).onTrue(commandFactory.groundAlgae());
 
-        //keypad.button(16).onTrue(commandFactory.exhaustNet()); // Temporary assignment
+        keypad.button(16).onTrue(commandFactory.exhaustNet()); // Temporary assignment
 
-        testingJoystick.a().onTrue(commandFactory.net());
-        testingJoystick.b().onTrue(commandFactory.exhaustNet());
+        //testingJoystick.a().onTrue(commandFactory.net());
+        //testingJoystick.b().onTrue(commandFactory.exhaustNet());
 
         //operatorJoystick.y().toggleOnTrue(pincer.holdIntakeCmd()); */
 
