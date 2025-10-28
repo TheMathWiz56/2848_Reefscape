47c4b84efd1a67157df9b43aab9059db11104507
Jesuit Student 26317 <26317@jcpstudents.org>
2025-10-28 16:57:40 -0500

    Early elevator movie change

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -70,7 +70,7 @@ public class TunerConstants {
     public static final Transform2d rightBranch = new Transform2d(0.46769, 0.16 + coralAlignmentShift, new Rotation2d(Math.PI));
     public static final Transform2d reefAlgae = new Transform2d(0.5,0.0,new Rotation2d(Math.PI));
 
-    public static final double kEarlyMove = .5;
+    public static final double kEarlyMove = .2;
     public static final Transform2d pathfindOffsetCenter = new Transform2d(1, 0.0, new Rotation2d(Math.PI));
     public static final Transform2d pathfindOffsetLeft = new Transform2d(1, -0.16 + coralAlignmentShift, new Rotation2d(Math.PI));
     public static final Transform2d pathfindOffsetRight = new Transform2d(1, 0.16 + coralAlignmentShift, new Rotation2d(Math.PI));
